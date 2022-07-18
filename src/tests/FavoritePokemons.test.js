4c7b11cb2b96ff969e2444db8126aa3793f36178
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-07-18 18:26:06 -0300

    Requisito 3 realizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o FavoritePokemons', () => {
+  test('É exibida a mensagem "No favorite pokemon found" se não tiver pokémons favoritos',
+    () => {
+      render(<FavoritePokemons />);
+      const noFavorite = screen.getByText('No favorite pokemon found');
+      expect(noFavorite).toBeInTheDocument();
+    });
+});

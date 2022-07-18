13a8404f3f7b448428c9829bcf1e89a1919b9933
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-07-18 18:34:35 -0300

    Requisito 4 realizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+
+/* Referencias:
+    https://testing-library.com/docs/vue-testing-library/cheatsheet/#search-types
+    https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+*/
+describe('Teste o NotFound', () => {
+  test('Teste se a página contém um heading h2 com o texto "Page requested not found 😭"',
+    () => {
+      render(<NotFound />);
+      const text = screen.getAllByRole('heading', { name: /Page requested not found/i });
+      expect(text).toBeDefined();
+    });
+  test('Teste se a página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    render(<NotFound />);
+    const texto = 'Pikachu crying because the page requested was not found';
+    const about = screen.getByAltText(texto);
+    expect(about).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

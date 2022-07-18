69177b3d6600b96c878e1415acd9cfe7a3a7d44d
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-07-18 19:04:03 -0300

    Requisito 1 realizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    test('Testa se o primeiro link possui o texto "Home"', async () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const linkHome = screen.getByRole('link', { name: /Home/i });
+      expect(linkHome).toBeInTheDocument();
+    });
+
+    test('Testa se o segundo link deve possuir o texto "About"', () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const about = screen.getByRole('link', { name: /About/i });
+      expect(about).toBeDefined();
+    });
+
+    test('Testa se o terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const favoritePokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+      expect(favoritePokemons).toBeDefined();
+    });
+  });
